6963f3e345df5ca065399d91b737ef70d1c032b7
Zack Werner <zackw0430@gmail.com>
2020-10-03 17:36:49 -0500

    updating deletefunction

--- script.js
+++ script.js
@@ -3,25 +3,48 @@ console.log("hello world!");
 
 $(document).ready(onReady);
 
-let employeeinfo = []
+let employeeinfo = ['dogs','cats', 'birds']
 
 function onReady(){
     console.log('hello from jQuery')
 
     $('#submitB').on('click', buttonClicked); //event listener | this is 'listening for click action. then it will run function.
     
-    $('#appendingEmp').append('<td>hello</td>')
+    // $('#appendingEmp').append('<td>hello</td>')
 
-
+   
+        // descendant selector
+    $('#appendingEmp').on('click', '.deleteBtn', deleteFunc);
 
 
 }
+
+
+function deleteFunc(){
+    console.log("delete");
+    $(this).parent().remove(); // the this event is saying I when x item is clicked do .remove()
+}
+
+
+
+
+
+
+
 
 
 
 function buttonClicked(){
     console.log('the button was clicked!')
 
+    let ulEmployees = $("#appendingEmp");
+
+    for (let i = 0 ; i < employeeinfo.length; i++){ 
+        ulEmployees.append(
+            `<td>  ${employeeinfo[i]}
+                 <button class= "deleteBtn"> DELETE </button> 
+            </td>`);
+    }
 
 }
 
