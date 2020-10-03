042ddf2d69c5a9b98df853f2402100af9f71639c
Zack Werner <zackw0430@gmail.com>
2020-10-03 14:53:36 -0500

    adding placesholders and button onto  html

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("hello world!");
+
+
+$(document).ready(onReady);
+
+
+function onReady(){
+    console.log('hello from jQuery')
+
+    $('#submitB').on('click', buttonClicked); //event listener | this is 'listening for click action. then it will run function.
+
+
+
+
+
+}
+
+
+
+function buttonClicked(){
+    console.log('the button was clicked!')
+
+
+}
