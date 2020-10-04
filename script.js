2e0dc354bc9286aa54062210ec29bebdd440527e
Zack Werner <zackw0430@gmail.com>
2020-10-04 18:36:48 -0500

    editing functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("hello world!");
 
 $(document).ready(onReady);
 
-let employeeinfo = []
+let employeeinfo = [] // array that is storing the objects from the loop / submit. 
 
 
 
@@ -13,49 +13,13 @@ function onReady(){
     $('#submitB').on('click', submitForm); //event listener
 
 
-    $('#submitB').on('click', buttonClicked); //event listener | this is 'listening for click action. then it will run function.
+    $('#submitB').on('click', appendfunc); //event listener | this is 'listening for click action. then it will run function.
     
   
-   
         // descendant selector
     $('#appendingEmp').on('click', '.deleteBtn', deleteFunc); // this is saying any child li's made will carry this deletefunction.
 
 }
-
-
-function deleteFunc(){
-    console.log("delete");
-    $(this).parent().parent().remove(); // the this event is saying I when x item is clicked do .remove()
-}
-
-
-
-
-function buttonClicked(){
-    console.log('buttonClicked Func');
-
-    let ulEmployees = $("#appendingEmp");
-
-    for (let i = 0 ; i < employeeinfo.length; i++){ 
-        console.log("woot");
-        
-        ulEmployees.append(`
-        <tr> 
-            <td> ${employeeinfo[i].nameKey} </td>
-            <td> ${employeeinfo[i].lastNKey} </td>
-            <td> ${employeeinfo[i].IDKey}</td> 
-            <td> ${employeeinfo[i].titleKey}</td> 
-            <td> ${employeeinfo[i].AnnSalary}</td> 
-            <td> <button class= "deleteBtn"> DELETE </button> </td>
-        </tr>`);
-            
-    }
-
-}
-
-
-
-
 
 
 function submitForm() {
@@ -63,7 +27,7 @@ function submitForm() {
     // this is where I want to grab input values;
     // getter .val()
     // setter .val('fills in')
-    //.text() is another getter and setter | if you want to GET something leave .() open | setter you need to have .('something') inside.
+
 
     let name = $('#firstName').val();
     console.log('fistname: ', name);
@@ -99,3 +63,46 @@ function submitForm() {
 
 
 }
+
+
+// function displayEmpl(){
+//     console.log('buttonClicked Func');
+
+//     let ulEmployees = $("#appendingEmp");
+
+
+// }
+
+
+
+function appendfunc(){
+
+    let ulEmployees = $("#appendingEmp");
+
+    ulEmployees.append(`
+    <tr> 
+        <td> ${employeeinfo[i].nameKey} </td>
+        <td> ${employeeinfo[i].lastNKey} </td>
+        <td> ${employeeinfo[i].IDKey}</td> 
+        <td> ${employeeinfo[i].titleKey}</td> 
+        <td> $ ${employeeinfo[i].AnnSalary}</td> 
+        <td> <button class= "deleteBtn"> DELETE </button> </td>
+    </tr>`);
+        
+}
+
+
+function deleteFunc(){
+    console.log("delete");
+    $(this).parent().parent().remove(); // the this event is saying I when x item is clicked do .remove()
+}
+
+
+
+
+
+
+
+
+
+
